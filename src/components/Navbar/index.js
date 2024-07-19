96825c7dace202b82fc7120c902d4edd4f547f10
SebastianVelasquez22901 <sbgamer43@gmail.com>
2024-07-18 20:12:51 -0600

    Actu

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -46,7 +46,7 @@ const Navbar = () => {
             <MobileLink href='#education' onClick={() => {
               setIsOpen(!isOpen)
             }}>Educacion</MobileLink>
-            <GitHubButton style={{padding: '10px 16px',background: `${theme.primary}`, color: 'white',width: 'max-content'}} href={Bio.github} target="_blank">Github Profile</GitHubButton>
+            <GitHubButton style={{padding: '10px 16px',background: `${theme.primary}`, color: 'white',width: 'max-content'}} href={Bio.github} target="_blank">Perfil github</GitHubButton>
           </MobileMenu>
         }
       </NavbarContainer>
