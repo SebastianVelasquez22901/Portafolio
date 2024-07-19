5bfc4a8fc86feac04c56caa28691fd157e5242e1
SebastianVelasquez22901 <sbgamer43@gmail.com>
2024-07-18 20:10:02 -0600

    Actu

--- src/data/constants.js
+++ src/data/constants.js
@@ -98,7 +98,7 @@ export const skills = [
       {
         name: "Git",
         image:
-          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+          "https://www.jaweewo.es/wp-content/uploads/2018/07/Git-Icon-1788C.png",
       },
       {
         name: "GitHub",
